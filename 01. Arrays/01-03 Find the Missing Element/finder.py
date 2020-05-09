f232e552bc4674df651ee65ac29f9262ce8471c9
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-09 14:22:56 +0300

    Improved solution, populating the dictionary with the 2nd array and finding the missing element.

--- 01. Arrays/01-03 Find the Missing Element/finder.py
+++ 01. Arrays/01-03 Find the Missing Element/finder.py
@@ -1,23 +1,20 @@
 from typing import List
+import collections
 
 
 def finder(arr1: List, arr2: List) -> int:
-    occurrences_left = {}
+    occurrences_left = collections.defaultdict(int)
 
-    # go through first array
+    # go through second array
     # populate dictionary with occurrences of each number
-    for num in arr1:
-        occurrences_left.setdefault(num, 0)
+    for num in arr2:
         occurrences_left[num] += 1
 
     # go through second array
-    for num in arr2:
-        # subtract occurrences
-        occurrences_left[num] -= 1
+    for num in arr1:
+        # if num not in dictionary
         if occurrences_left[num] == 0:
-            # if occurrence is 0 remove from dict
-            del occurrences_left[num]
-
-    missing_element = next(iter(occurrences_left))
-
-    return missing_element
+            return num
+        else:
+            # subtract occurrences
+            occurrences_left[num] -= 1
