253b702cec06c4924eafc51d5ac7ec07c29b21d4
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-14 17:47:55 +0300

    Add stack implementation and solution

---
+++ 02. Stacks, Queues and Deques/02-01 Implement a Stack/readme.md
@@ -0,0 +1,21 @@
+# Implement a Stack
+
+## Problem
+
+Implement a stack. It should have the methods:
+
+- Check if its empty
+- Push a new item
+- Pop an item
+- Peek at the top item
+- Return the size
+
+## Code
+
+Create your solution in the form:
+
+```python
+class Stack(object):
+    # Fill out the Stack Methods here
+    pass
+```
---
+++ 02. Stacks, Queues and Deques/02-01 Implement a Stack/stack.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+
+class Stack(object):
+    def __init__(self) -> None:
+        self.items = []
+
+    def push(self, item: Any) -> None:
+        # Push a new item
+        self.items.append(item)
+
+    def pop(self) -> Any:
+        # Pop an item
+        return self.items.pop()
+
+    def peek(self) -> Any:
+        # Peek at the top item
+        if self.is_empty():
+            raise Exception("Stack empty!")
+        return self.items[-1]
+
+    def size(self) -> int:
+        # Return the size
+        return len(self.items)
+
+    def is_empty(self) -> bool:
+        # Checks if its empty
+        return self.size() == 0
