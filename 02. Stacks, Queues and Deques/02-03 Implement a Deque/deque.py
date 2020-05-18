553af260c66106f4b1a5b476816c615341a03bdb
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-18 18:20:44 +0300

    Add deque implementation

---
+++ 02. Stacks, Queues and Deques/02-03 Implement a Deque/deque.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+
+class Deque(object):
+    def __init__(self) -> None:
+        self.items = []
+
+    def size(self) -> int:
+        # Checks the Size
+        return len(self.items)
+
+    def is_empty(self) -> bool:
+        # Checks if its empty
+        return self.size() == 0
+
+    def add_front(self, element) -> None:
+        self.items.insert(0, element)
+
+    def add_rear(self, element) -> None:
+        self.items.append(element)
+
+    def remove_front(self) -> Any:
+        return self.items.pop(0)
+
+    def remove_rear(self) -> Any:
+        return self.items.pop()
