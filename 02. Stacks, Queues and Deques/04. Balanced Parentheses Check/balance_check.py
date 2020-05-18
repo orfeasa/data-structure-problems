4165bd1144d1d78bc3a417f66b041918dbc49a12
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-18 18:46:02 +0300

    add balance check solution

---
+++ 02. Stacks, Queues and Deques/04. Balanced Parentheses Check/balance_check.py
@@ -0,0 +1,25 @@
+def balance_check(s: str) -> bool:
+    stack = []
+
+    # set up pairs so that keys are closing and values are opening
+    pairs = {
+        "]": "[",
+        "}": "{",
+        ")": "(",
+    }
+
+    for char in s:
+        # if it's an opening
+        if char in pairs.values():
+            # add to the stack
+            stack.append(char)
+        # if it's a closing
+        elif char in pairs:
+            # if the stack is not empty, and the closing corresponds to the last opening
+            if len(stack) != 0 and stack[-1] == pairs[char]:
+                # remove the respective opening
+                stack.pop()
+            else:
+                return False
+
+    return len(stack) == 0
