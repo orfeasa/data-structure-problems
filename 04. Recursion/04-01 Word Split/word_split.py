c9c844bc18c213364fee35683346ece5e24f34de
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-28 21:45:32 +0300

    Minor formatting edit

--- 04. Recursion/04-01 Word Split/word_split.py
+++ 04. Recursion/04-01 Word Split/word_split.py
@@ -4,7 +4,7 @@ from typing import List
 def word_split(phrase: str, list_of_words: List, output: List = []) -> List:
     # Check for each word in the list if it fits the beginning of the phrase
     for word in list_of_words:
-        if phrase[: len(word)] == word:
+        if phrase[:len(word)] == word:
             if len(phrase) > len(word):
                 return word_split(phrase[len(word):], list_of_words, output + [word])
             else:
