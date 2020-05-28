c75b05d30df2171f2f8d82afd250fe3f6f56d049
Orfeas Antoniou <orfeasa@gmail.com>
2020-05-28 20:08:55 +0300

    Add word split solution

---
+++ 04. Recursion/04-01 Word Split/word_split.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def word_split(phrase: str, list_of_words: List, output: List = []) -> List:
+    # Check for each word in the list if it fits the beginning of the phrase
+    for word in list_of_words:
+        if phrase[: len(word)] == word:
+            if len(phrase) > len(word):
+                return word_split(phrase[len(word):], list_of_words, output + [word])
+            else:
+                return output + [word]
+
+    return []
