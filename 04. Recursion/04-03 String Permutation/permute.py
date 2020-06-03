9f7250775273c9c8de427d36ece48fe0e7227a1d
Orfeas Antoniou <orfeasa@gmail.com>
2020-06-03 21:38:55 +0300

    Add alternative solution to string permutation problem

--- 04. Recursion/04-03 String Permutation/permute.py
+++ 04. Recursion/04-03 String Permutation/permute.py
@@ -8,12 +8,32 @@ def permute(s: str) -> List[str]:
 
     # Recursively call the permutations of the substring starting after the 1st character
     sublist = permute(s[1:])
-    new_list = []
+    out = []
     # for each permutations of the substring create a new one, where the first
     # character is "injected" in between characters of the previous one
     for substr in sublist:
         for pos in range(len(substr)):
-            new_list.append(substr[:pos] + s[0] + substr[pos:])
-        new_list.append(substr + s[0])
+            out.append(substr[:pos] + s[0] + substr[pos:])
+        out.append(substr + s[0])
 
-    return new_list
+    return out
+
+
+def permute2(s: str) -> List[str]:
+    out = []
+
+    # Base Case
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        # For every letter in string
+        for i, letter in enumerate(s):
+
+            # For every permutation without that letter
+            for perm in permute(s[:i] + s[i + 1 :]):
+
+                # Add it to output
+                out += [letter + perm]
+
+    return out
